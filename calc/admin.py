c622de2862bf13777987395e513b49c749df3d49
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-10-29 18:37:46 +0900

    models.pyに必要なデータベース情報を記述

--- calc/admin.py
+++ calc/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Status, Subject, Category, Cource, Cource_Subject
 
-# Register your models here.
+
+admin.site.register(Status)
+admin.site.register(Subject)
+admin.site.register(Category)
+admin.site.register(Cource)
+admin.site.register(Cource_Subject)
--- calc/models.py
+++ calc/models.py
@@ -1,3 +1,109 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    class Meta:
+        verbose_name = 'ステータス'
+        verbose_name_plural = 'ステータス'
+        unique_together = ('user', 'subject')
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    subject = models.ForeignKey(
+        'Subject',
+        on_delete=models.CASCADE,
+    )
+    doing = models.BooleanField(
+        default=False,
+    )
+    done = models.BooleanField(
+        default=False,
+    )
+
+    def __str__(self):
+        return str(self.user) + " " + str(self.subject)
+
+
+class Subject(models.Model):
+    class Meta:
+        verbose_name = '授業科目'
+        verbose_name_plural = '授業科目'
+
+    subject_id = models.IntegerField(
+        primary_key=True,
+        default=None,
+        unique=True,
+    )
+    subject_name = models.CharField(
+        max_length=30,
+        unique=True,
+    )
+    credits = models.IntegerField(
+        default=2,
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.subject_name
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'カテゴリー'
+        verbose_name_plural = 'カテゴリー'
+
+    category_name = models.CharField(
+        max_length=30,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.category_name
+
+
+class Cource(models.Model):
+    class Meta:
+        verbose_name = 'コース'
+        verbose_name_plural = 'コース'
+
+    cource_abbreviation = models.CharField(
+        max_length=10,
+        primary_key=True,
+        unique=True,
+    )
+    cource_name = models.CharField(
+        max_length=30,
+        default=None,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.cource_abbreviation
+
+
+class Cource_Subject(models.Model):
+    class Meta:
+        verbose_name = '科目のコース'
+        verbose_name_plural = '科目のコース'
+        unique_together = ('subject', 'cource')
+
+    subject = models.ForeignKey(
+        'Subject',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    cource = models.ForeignKey(
+        'Cource',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        return str(self.subject) + " " + str(self.cource)
